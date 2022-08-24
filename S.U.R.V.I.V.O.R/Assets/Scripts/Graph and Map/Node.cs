1eacee15b3f51aca10b2e4b878b5515fd93dc510
pavel <pavel.mokr2005@gmail.com>
2022-08-24 18:36:22 +0500

    Поиск ближайшей ноды

--- S.U.R.V.I.V.O.R/Assets/Scripts/Graph and Map/Node.cs
+++ S.U.R.V.I.V.O.R/Assets/Scripts/Graph and Map/Node.cs
@@ -13,6 +13,7 @@ namespace Graph_and_Map
     {
         public List<Node> neighborhoods;
         public LocationType location;
+        private LineRenderer line;
         public Vector2 positionIn2D => new(transform.position.x,transform.position.z);
         public IEnumerator<(Node start, Node end)> GetEnumerator()
         {
@@ -21,5 +22,21 @@ namespace Graph_and_Map
                 yield return (this, neighborhood);
         }
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private void Awake()
+        {
+            var index = 0;
+            line = GetComponent<LineRenderer>(); 
+            neighborhoods.RemoveAll(node => node == null);
+            line.positionCount = neighborhoods.Count * 2;
+            foreach (var neighborhood in neighborhoods)
+            {
+                line.SetPosition(index,transform.position);
+                index++;
+                line.SetPosition(index,neighborhood.transform.position);
+                index++;
+            }
+        }
+        
     }
 }
