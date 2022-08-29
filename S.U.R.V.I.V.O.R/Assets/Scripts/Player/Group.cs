4b016b4c9728c134501d8f9caf80237c8d06c854
Saw4uk <87923228+Saw4uk@users.noreply.github.com>
2022-08-29 15:01:04 +0500

    Group Class

---
+++ S.U.R.V.I.V.O.R/Assets/Scripts/Player/Group.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graph_and_Map;
+using UnityEngine;
+
+namespace Player
+{
+    public class Group
+    {
+
+        public Node CurrentNode;
+        public Vector3 Position;
+        private IPlayer[] GroupMembers;
+        public int GroupPossibleRange;
+
+
+        public Group(IPlayer[] groupMembers, Node currentNode)
+        {
+            GroupMembers = new IPlayer[4];
+            for (var i = 0; i < 4; i++)
+            {
+                GroupMembers[i] = groupMembers[i];
+            }
+            CurrentNode = currentNode;
+        }
+
+        public void Start()
+        {
+            InputAggregator.OnTurnEndEvent += OnTurnEnd;
+        }
+
+        private void OnTurnEnd()
+        {
+            SubtractWater();
+            SubtractSatiety();
+        }
+
+        public void MoveGroup(Node TargetNode)
+        {
+            SubtractEnergy();
+            CurrentNode = TargetNode;
+            //Переместить группу в ноду, изменяя значение CurrentNode по мере продвижения
+        }
+
+        private void SubtractEnergy()
+        {
+            foreach (var groupMember in GroupMembers)
+            {
+                groupMember.Energy--;
+            }
+        }
+
+        private void SubtractWater()
+        {
+            foreach (var groupMember in GroupMembers)
+            {
+                groupMember.Water--;
+            }
+        }
+
+        private void SubtractSatiety()
+        {
+            foreach (var groupMember in GroupMembers)
+            {
+                groupMember.Satiety--;
+            }
+        }
+    }
+}
