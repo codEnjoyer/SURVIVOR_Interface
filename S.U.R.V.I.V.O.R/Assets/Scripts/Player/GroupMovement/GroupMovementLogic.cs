7013f353dbed35edaa65eb387719dfc95db2d1ba
Saw4uk <87923228+Saw4uk@users.noreply.github.com>
2022-08-31 12:54:04 +0500

    Advanced Group Moving

--- S.U.R.V.I.V.O.R/Assets/Scripts/Player/GroupMovement/GroupMovementLogic.cs
+++ S.U.R.V.I.V.O.R/Assets/Scripts/Player/GroupMovement/GroupMovementLogic.cs
@@ -17,8 +17,14 @@ namespace Player
             MovingFromAToB
         }
 
+        //Обьекты Ходов
+        public GameObject FirstTurnObject;
+        public GameObject SecondTurnObject;
+        public GameObject ThirdTurnObject;
+        private LineRenderer FirstTurnObjectLineRenderer;
+        private LineRenderer SecondTurnObjectLineRenderer;
+        private LineRenderer ThirdTurnObjectLineRenderer;
 
-        public GameObject ObjToSpawn;
         public DotGraph Graph;
 
         public Stage CurrentStage;
@@ -38,6 +44,9 @@ namespace Player
             CurrentGroupEndurance = GroupGameLogic.MaxGroupEndurance;
             LineRenderer = GetComponent<LineRenderer>();
             LineRenderer.positionCount = 0;
+            FirstTurnObjectLineRenderer = FirstTurnObject.GetComponent<LineRenderer>();
+            SecondTurnObjectLineRenderer = SecondTurnObject.GetComponent<LineRenderer>();
+            ThirdTurnObjectLineRenderer = ThirdTurnObject.GetComponent<LineRenderer>();
         }
         
         public void Start()
@@ -85,9 +94,7 @@ namespace Player
                     progress = 0;
                     if (Way.Count == 0 || CurrentGroupEndurance == 0)
                     {
-                        CurrentStage = Stage.Sleeping;
-                        LineRenderer.positionCount = 0;
-                        ObjToSpawn.transform.position = new Vector3(0, -10, 0);
+                        ClearWay();
                     }
                     else
                     {
@@ -102,17 +109,92 @@ namespace Player
 
         private void DrawWay(List<Node> list)
         {
-            LineRenderer.positionCount = list.Count;
-            LineRenderer.SetPositions(list.Select(x => x.transform.position + new Vector3(0, 0.5f, 0)).ToArray());
-            for (var nodeNumber = 0; nodeNumber < list.Count; nodeNumber++)
+            var firstList = new List<Vector3>();
+            var secondList = new List<Vector3>();
+            var thirdList = new List<Vector3>();
+            var lastList = new List<Vector3>();
+            for (var i = 0; i < list.Count; i++)
             {
-                if (nodeNumber <= CurrentGroupEndurance)
+                var element = list[i].transform.position;
+                if (i < CurrentGroupEndurance)
                 {
-                    ObjToSpawn.transform.position = list[nodeNumber].transform.position;
+                    firstList.Add(element);
                 }
+                else if(i == CurrentGroupEndurance)
+                {
+                    firstList.Add(element);
+                    secondList.Add(element);
+                }
+                else if (i < CurrentGroupEndurance + GroupGameLogic.MaxGroupEndurance)
+                {
+                    secondList.Add(element);
+                }
+                else if (i == CurrentGroupEndurance + GroupGameLogic.MaxGroupEndurance)
+                {
+                    thirdList.Add(element);
+                    secondList.Add(element);
+                }
+                else if (i < CurrentGroupEndurance + 2 * GroupGameLogic.MaxGroupEndurance)
+                {
+                    thirdList.Add(element);
+                }
+                else if (i == CurrentGroupEndurance + 2 * GroupGameLogic.MaxGroupEndurance)
+                {
+                    thirdList.Add(element);
+                    lastList.Add(element);
+                }
+                else
+                {
+                    lastList.Add(element);
+                }
+            }
+
+            DrawLineRenderer(LineRenderer,FirstTurnObject,firstList);
+            DrawLineRenderer(FirstTurnObjectLineRenderer, SecondTurnObject, secondList);
+            DrawLineRenderer(SecondTurnObjectLineRenderer,ThirdTurnObject, thirdList);
+            DrawLineRenderer(ThirdTurnObjectLineRenderer,lastList);
+        }
+
+        private void DrawLineRenderer(LineRenderer lineRenderer, GameObject turnObject, List<Vector3> nodes)
+        {
+            if (nodes.Count > 0)
+            {
+                lineRenderer.positionCount = nodes.Count;
+                lineRenderer.SetPositions(nodes.Select(x => x + new Vector3(0, 0.5f, 0)).ToArray());
+                turnObject.transform.position = nodes.Last();
+            }
+            else
+            {
+                turnObject.transform.position = new Vector3(0, -10, 0);
+                lineRenderer.positionCount = 0;
             }
         }
 
+        private void DrawLineRenderer(LineRenderer lineRenderer, List<Vector3> nodes)
+        {
+            if (nodes.Count > 0)
+            {
+                lineRenderer.positionCount = nodes.Count;
+                lineRenderer.SetPositions(nodes.Select(x => x + new Vector3(0,0.5f,0)).ToArray());
+            }
+            else
+            {
+                lineRenderer.positionCount = 0;
+            }
+        }
+
+        private void ClearWay()
+        {
+            CurrentStage = Stage.Sleeping;
+            LineRenderer.positionCount = 0;
+            FirstTurnObjectLineRenderer.positionCount = 0;
+            SecondTurnObjectLineRenderer.positionCount = 0;
+            ThirdTurnObjectLineRenderer.positionCount = 0;
+
+            FirstTurnObject.transform.position = new Vector3(0, -10, 0);
+            SecondTurnObject.transform.position = new Vector3(0, -10, 0);
+            ThirdTurnObjectLineRenderer.transform.position = new Vector3(0, -10, 0);
+        }
         private void OnTurnEnd()
         {
             CurrentGroupEndurance = GetComponent<GroupGameLogic>().MaxGroupEndurance;
