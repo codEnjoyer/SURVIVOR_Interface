a553ebeeef5fa06dddd25a07a66d2101ba64e84b
Pavel <pavel.mokr2005@gmail.com>
2022-10-23 16:34:15 +0500

    stateMachine

---
+++ S.U.R.V.I.V.O.R/Assets/Scripts/Interface/InterfaceStates/CharacterPanelActive.cs
@@ -0,0 +1,19 @@
+﻿public class CharacterPanelActive : InterfaceState
+{
+    public CharacterPanelActive(InterfaceController contr, StateMachine sm)
+        : base(contr, sm)
+    {
+    }
+
+    public override void Enter()
+    {
+        // if (sm.CurrentState == this)
+        //    sm.ChangeState(contr.nothingActive);
+        // contr.CharactersButtonsLayer.SetActive(true);
+    }
+
+    public override void Exit()
+    {
+        // contr.CharactersButtonsLayer.SetActive(false);
+    }
+}
---
+++ S.U.R.V.I.V.O.R/Assets/Scripts/Interface/InterfaceStates/NothingActive.cs
@@ -0,0 +1,17 @@
+﻿
+public class NothingActive: InterfaceState
+{
+    public NothingActive(InterfaceController contr, StateMachine sm) 
+        : base(contr, sm)
+    {
+    }
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
