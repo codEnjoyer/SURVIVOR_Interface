91c88f24f224893ef6e6c1c2facaaf905b48960a
Pavel <pavel.mokr2005@gmail.com>
2022-11-29 18:00:20 +0500

    SizeParserBeta

--- S.U.R.V.I.V.O.R/Assets/Scripts/GoogleSheetLink/SheetParser.cs
+++ S.U.R.V.I.V.O.R/Assets/Scripts/GoogleSheetLink/SheetParser.cs
@@ -1,22 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace GoogleSheetLink
 {
     public class SheetParser: MonoBehaviour
     {
+        // private Dictionary<string, Func<object[], object>> сonverter = new()
+        // {
+        //     {"BaseItem", param => }
+        // };
         [SerializeField] private string sheetName;
         [SerializeField] private string from;
         [SerializeField] private string to;
-        private const string SpreadsheetId = "12o3fSTiRqjt2EpLmurYA9KE_DWGaghkFuJkT4jzL09g";
-        private const string SecretJsonFileName = "JsonKey.json";
+        
         private GoogleSheetHelper googleSheetHelper;
         private string range;
 
+
         private void Awake()
         {
-            googleSheetHelper = new GoogleSheetHelper(SpreadsheetId, SecretJsonFileName);
+
+            googleSheetHelper = new GoogleSheetHelper("12o3fSTiRqjt2EpLmurYA9KE_DWGaghkFuJkT4jzL09g", "JsonKey.json");
             range = $"{sheetName}!{from}:{to}";
+        }
+
+        private void Parse(List<List<string>> table)
+        {
+            foreach (var row in table)
+            {
+                foreach (var cell in row)
+                {
+                    
+                }
+            }
         }
     }
 }
---
+++ S.U.R.V.I.V.O.R/Assets/Scripts/GoogleSheetLink/SizeParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace GoogleSheetLink
+{
+    public class SizeParser
+    {
+        private readonly string absolutePath;
+        private readonly string relativePath;
+        private readonly Regex regex;
+        private List<string> sizeObjectsFileNames;
+
+        public SizeParser()
+        {
+            regex = new Regex(@"\d+");
+            absolutePath = $@"{Application.dataPath}/Resources/InventorySizeObjects";
+            relativePath = @"Assets/Resources/InventorySizeObjects";
+            FindSizeObjects();
+        }
+
+        public Size Parse(string sizeObjectName)
+        {
+            sizeObjectName = $"{sizeObjectName}.asset";
+            var objPath = $@"{absolutePath}/{sizeObjectName}";
+            if (sizeObjectsFileNames.Contains(sizeObjectName))
+                return (Size) Resources.Load(objPath);
+            else
+                return CreateSizeObject(sizeObjectName);
+        }
+
+        private Size CreateSizeObject(string sizeObjectName)
+        {
+            var findData = regex.Matches(sizeObjectName)
+                .Select(x => int.Parse(x.ToString()))
+                .ToArray();
+            if (findData.Length != 2)
+                throw new ArgumentException("Неверное форматирование размера предмета (Size). Объект не был создан.");
+            var sizeObj = new Size(findData[0], findData[1]);
+
+            var objPath = AssetDatabase.GenerateUniqueAssetPath(relativePath);
+            AssetDatabase.CreateAsset(sizeObj, objPath);
+            return sizeObj;
+        }
+
+        private void FindSizeObjects()
+        {
+            sizeObjectsFileNames = new List<string>();
+            var regex = new Regex(@"[.]asset\z");
+            foreach (var file in Directory.EnumerateFiles(absolutePath))
+                if(regex.IsMatch(file))
+                    sizeObjectsFileNames.Add(file);
+        }
+    }
+}
