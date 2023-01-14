71300e96ba44733b25b44d77dbdeec1659ca0a08
KirillMasalov <masalov.kirill03@mail.ru>
2023-01-14 23:50:51 +0500

    SegmentProgressBar

---
+++ S.U.R.V.I.V.O.R/Assets/Resources/Interface/TestUI/SegmentProgressBar.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class SegmentProgressBar : MonoBehaviour
+{
+    [SerializeField] private GameObject segmentPrefab;
+    [SerializeField] private Color segmentBorderColor;
+    [SerializeField] private Color segmentFillColor;
+    [SerializeField] private int maxValue = 10;
+    [SerializeField] private int value;
+
+    private void Awake()
+    {
+        for(var i = 0; i < maxValue; i++)
+        {
+            var rectSize = GetComponent<RectTransform>().rect.size;
+            GetComponent<GridLayoutGroup>().cellSize = new Vector2(rectSize.x / (float)maxValue, rectSize.y);
+            var segmentObj = Instantiate(segmentPrefab, transform);
+            var segment = segmentObj.transform;
+            segment.GetComponent<Image>().color = segmentBorderColor;
+            segment.Find("Fill").transform.GetComponent<Image>().color = segmentFillColor;
+            if (i >= value)
+                segment.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetValue(int value)
+    {
+        if (value > maxValue)
+            throw new ArgumentException($"Значение больше максимального (Передано: {value} > {maxValue})");
+        var i = 0;
+        foreach (var segmentObj in transform)
+        {
+            (segmentObj as Transform).gameObject.SetActive(i < value);
+            i++;
+            if(i >= maxValue)
+                break;
+        }
+    }
+}
